4597b1472dd2c73d5f4994fb10a2fd4cbae215c0
pradnya <pradnyasg94@gmail.com>
2024-01-30 20:35:13 -0500

    Parameter added

--- JenkinsTutorial/src/test/java/Demo/MyJenkin1.java
+++ JenkinsTutorial/src/test/java/Demo/MyJenkin1.java
@@ -3,17 +3,21 @@ package Demo;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class MyJenkin1 {
+	@Parameters("Browser")
 	@Test
-	public void show() {
+	public void show(String browserName) {
+		System.out.println("Use chrome browser");
+		if(browserName.contains("Chrome")) {
 		System.setProperty("webdriver.chromedriver.driver", "D:\\Driver\\chromedriver-win64");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.facebook.com/");
 		String title= driver.getTitle();
 		System.out.println(title);
-		
+		}
 		
 	}
 
